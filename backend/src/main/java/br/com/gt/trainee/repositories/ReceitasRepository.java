7fd71f7cae4edaf803220da5200d71fa92abc5d0
Daniel Vidal <danielsv465@hotmail.com>
2021-06-04 10:54:21 -0300

    static pages created

--- backend/src/main/java/br/com/gt/trainee/repositories/ReceitasRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.gt.trainee.repositories;
-
-import br.com.gt.trainee.models.Receitas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceitasRepository extends JpaRepository<Receitas, Long> {
-}
